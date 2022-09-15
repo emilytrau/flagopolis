96223fb5aeea142e61800142331777d46b4e800d
Emily Trau <me@angus.ws>
2022-09-15 17:38:25 +0000

    Remove sqlite for now, incompatible with Deno Deploy

---
+++ flagopolis/db/json/mod.ts
@@ -0,0 +1,5 @@
+import { DatabaseConnector } from "../interface.ts";
+
+export class JSONDatabase implements DatabaseConnector {
+  start = async () => {};
+}
--- flagopolis/db/mod.ts
+++ flagopolis/db/mod.ts
@@ -1,5 +1,5 @@
-import { SQLiteDatabase } from "./sqlite/mod.ts";
+import { JSONDatabase } from "./json/mod.ts";
 import { DatabaseConnector } from "./interface.ts";
 
-const connector: DatabaseConnector = new SQLiteDatabase();
+const connector: DatabaseConnector = new JSONDatabase();
 export default connector;
