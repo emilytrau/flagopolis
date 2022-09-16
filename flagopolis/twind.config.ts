3a0c4f073ed782751018cf6ea9db6c833ce9d2c4
Emily Trau <me@angus.ws>
2022-09-16 00:29:11 +0000

    Try this font

--- flagopolis/twind.config.ts
+++ flagopolis/twind.config.ts
@@ -10,11 +10,13 @@ export default {
         secondary: colors.blueGray,
       },
       fontFamily: {
-        sans: "Cabin, sans-serif",
+        sans:
+          "'Encode Sans', ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, 'Noto Sans', sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji'",
       },
     },
   },
   preflight: {
-    "@import": "url(https://fonts.bunny.net/css?family=cabin:400,500,600,700)",
+    "@import":
+      "url(https://fonts.bunny.net/css?family=encode-sans:100,200,300,400,500,600,700,800,900)",
   },
 } as Options;
